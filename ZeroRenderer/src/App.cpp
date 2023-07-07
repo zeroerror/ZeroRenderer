e0fab2efa6ed19c0c34a1a54b3ea7f1c857f2d8d
zenghong <1668118834@qq.com>
2023-07-07 11:42:17 +0800

    <feature> Editor - Project面板同步文件夹显示

--- ZeroRenderer/src/App.cpp
+++ ZeroRenderer/src/App.cpp
@@ -15,6 +15,10 @@ const int EDITOR_WINDOW_HEIGHT = 1080;
 const ImVec2 EDITOR_WINDOW_PROJECT_POSITION = ImVec2(0, EDITOR_WINDOW_HEIGHT * (1.8f / 3.0f));
 const int EDITOR_WINDOW_PROJECT_WIDTH = EDITOR_WINDOW_WIDTH;
 const int EDITOR_WINDOW_PROJECT_HEIGHT = EDITOR_WINDOW_HEIGHT / 3.0f;
+const int EDITOR_WINDOW_PROJECT_LEFT_COLUNM_WIDTH = EDITOR_WINDOW_PROJECT_WIDTH / 4.0f;
+const int EDITOR_WINDOW_PROJECT_LEFT_COLUNM_HEIGHT = EDITOR_WINDOW_PROJECT_HEIGHT;
+const int EDITOR_WINDOW_PROJECT_RIGHT_COLUNM_WIDTH = EDITOR_WINDOW_WIDTH - EDITOR_WINDOW_PROJECT_LEFT_COLUNM_WIDTH;
+const int EDITOR_WINDOW_PROJECT_RIGHT_COLUNM_HEIGHT = EDITOR_WINDOW_PROJECT_HEIGHT;
 
 // ********************** EDITOR CACHE **********************
 AssetTreeNode* _rootNode;
@@ -33,16 +37,17 @@ void ImGui_ShowProjectPanel(AssetTreeNode* node, string dir, float xOffset) {
 	string assetName = node->assetName;
 	const char* assetNamec = assetName.c_str();
 	string choosedPath = dir + assetName;
+	ImGui::PushID(1);
 	if (ImGui::Selectable(assetNamec, choosedPath == _curProjectChoosedPath)) {
 		_curProjectChoosedPath = choosedPath;
 		double nowClickTime = glfwGetTime();
 		double clickTimeOffset = nowClickTime - _assetClickTime;
+		_assetClickTime = nowClickTime;
 		if (clickTimeOffset < 0.2f) {
 			node->isExpanded = !node->isExpanded;
-			std::cout << " choosedPath  " + choosedPath << std::endl;
 		}
-		_assetClickTime = nowClickTime;
 	}
+	ImGui::PopID();
 
 	if (!node->isExpanded) {
 		ImGui::Unindent(xOffset);
@@ -67,16 +72,32 @@ void ImGui_ShowProjectDetailsPanel(const AssetTreeNode* node) {
 	ImGui::Text(_curProjectChoosedPath.c_str());
 	ImGui::Spacing();
 	ImGui::Indent(10.0f);
+	unsigned int colunmCount = EDITOR_WINDOW_PROJECT_HEIGHT;
+	ImGui::PushID(2);
 	for (auto kvp : node->childNodes) {
 		AssetTreeNode* node = kvp.second;
 		if (node->isDir) {
-			ImGui::Image(reinterpret_cast<ImTextureID>(_texture_id), ImVec2(64, 64)); // 调整图标大小
+			ImGui::Image(reinterpret_cast<ImTextureID>(_texture_id), ImVec2(32, 32)); // 调整图标大小
+			ImGui::SameLine();
 		}
-		const char* assetName = node->assetName.c_str();
-		if (ImGui::Selectable(assetName, assetName == _curProjectDetailsChoosedPath)) {
-			_curProjectDetailsChoosedPath = assetName;
+		const char* assetNamec = node->assetName.c_str();
+		if (ImGui::Selectable(assetNamec, assetNamec == _curProjectDetailsChoosedPath, ImGuiSelectableFlags_None, ImVec2(0, 0))) {
+			_curProjectDetailsChoosedPath = assetNamec;
+			double nowClickTime = glfwGetTime();
+			double clickTimeOffset = nowClickTime - _assetClickTime;
+			_assetClickTime = nowClickTime;
+			if (clickTimeOffset < 0.2f) {
+				AssetTreeNode* detailNode;
+				_curProjectChoosedPath += "/" + _curProjectDetailsChoosedPath;
+				if (_rootNode->TryGetNodeByPath(_curProjectChoosedPath, detailNode)) {
+					if (detailNode->isDir) {
+						detailNode->isExpanded = true;
+					}
+				}
+			}
 		}
 	}
+	ImGui::PopID();
 }
 
 int main() {
@@ -125,6 +146,8 @@ int main() {
 		ImGui::Begin("Project", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar);
 
 		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, EDITOR_WINDOW_PROJECT_LEFT_COLUNM_WIDTH);
+		ImGui::SetColumnWidth(1, EDITOR_WINDOW_PROJECT_RIGHT_COLUNM_WIDTH);
 		ImGui_ShowProjectPanel();
 		ImGui::NextColumn();
 
